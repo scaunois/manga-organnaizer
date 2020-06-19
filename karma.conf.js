76c18ec53af48d4cf31484db5a6873e07f5504a7
scaunois <sebastiencaunois@gmail.com>
2020-06-19 23:51:20 +0200

    fix : pressing Enter didn't save the new value for a field

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/manga-organnaizer-frontend'),
+      dir: require('path').join(__dirname, './coverage/manga-organnaizer'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
